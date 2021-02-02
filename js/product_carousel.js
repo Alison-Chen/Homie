40ba499d0e4f838def99b92fd866a6d1c12ec789
Alison-Chen <jane2627n@gmail.com>
2021-02-02 10:04:39 +0800

    product_carousol

--- js/product_carousel.js
+++ js/product_carousel.js
@@ -1,22 +1,27 @@
-// $(".product").owlCarousel({
-//     loop: false, // 循環播放
-//     margin: 10, // 外距 10px
-//     nav: true, // 顯示點點
-//     dots: false,
-//     autoplay: false,
-//     // autoplayTimeout:3000,
-//     // autoplayHoverPause:true,
-//     navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-//     responsive: {
-//       0: {
-//         items: 1 // 螢幕大小為 0~600 顯示 1 個項目
-//       },
-//       600: {
-//         items: 1 // 螢幕大小為 600~1000 顯示 3 個項目
-//       },
-//       1000: {
-//         items: 4, // 螢幕大小為 1000 以上 顯示 5 個項目
-//         stagePadding: 400
-//       }
-//     }
-//   });
+$(".product").owlCarousel({
+    loop: false, // 循環播放
+    margin: 10, // 外距 10px
+    nav: true, // 顯示點點
+    dots: false,
+    autoplay: false,
+    // autoplayTimeout:3000,
+    // autoplayHoverPause:true,
+    navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    responsive: {
+      0: {
+        items: 2,
+        stagePadding: 100 // 螢幕大小為 0~600 顯示 1 個項目
+      },
+      320:{
+        items: 1,
+        stagePadding: 50 
+      },
+      768: {
+        items: 2,
+        stagePadding: 100// 螢幕大小為 600~1000 顯示 3 個項目
+      },
+      1000: {
+        items: 6, // 螢幕大小為 1000 以上 顯示 5 個項目
+      }
+    }
+  });
